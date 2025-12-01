fb4b20e310690418e72ece1d207919d2ffc53166
xyzHooDlEzyx <93081557+xyzHooDlEzyx@users.noreply.github.com>
2025-12-01 16:50:19 +0200

    made code fixes and added .env file

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-import read_values
+import read_values, json, requests, os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+aws_url = os.getenv('AWS_URL')
+aws_key = os.getenv('AWS_KEY')
 
 def send_data_to_aws(data):
     print(f"Sending data to AWS: {data}")
+    try:
+        response = requests.post(aws_url, json=data)
+        print(response.status_code, response.text)
+    except requests.exceptions.RequestException as e:
+        print(f"Error sending data to AWS: {e}")
+
 
 if __name__ == "__main__":
     read_values.run()
